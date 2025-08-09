a27544577ef8f7bacfeed5956e2bfa3eb598d730
R0GGER <r.meijer@hibbit.nl>
2025-08-10 01:36:53 +0200

    Update zimaos_zimacube.sh
    VERSION="1.4.3"

--- zimaos_zimacube.sh
+++ zimaos_zimacube.sh
@@ -4,7 +4,7 @@
 # bash -c "$(wget -qLO - https://raw.githubusercontent.com/R0GGER/proxmox-zimaos/refs/heads/main/zimaos_zimacube.sh)"
 
 # ZimaOS version
-VERSION="1.4.1"
+VERSION="1.4.3"
 
 # Variables
 URL="https://github.com/IceWhaleTech/ZimaOS/releases/download/$VERSION"
